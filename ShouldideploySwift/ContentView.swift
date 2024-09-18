77f6b1f69b38fbcf39d611e7c4168509d4c99349
Leonardo Benedeti <leonardobenedeti@gmail.com>
2024-09-18 17:31:00 -0300

    Initial Commit

---
+++ ShouldideploySwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShouldideploySwift
+//
+//  Created by Leonardo Benedeti on 18/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShouldideploySwift/ShouldideploySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShouldideploySwiftApp.swift
+//  ShouldideploySwift
+//
+//  Created by Leonardo Benedeti on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ShouldideploySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
